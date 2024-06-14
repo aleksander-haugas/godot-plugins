fabf4b97ca706cec086d4756b6d4e59d8ce2baab
Aleksander Haugas <aleksander.haugas@outlook.com>
2024-06-14 20:25:23 +0200

    Create cryptolib.gd

---
+++ cryptolib/cryptolib.gd
@@ -0,0 +1,11 @@
+@tool
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ##
+##  AES implementation in GDscript                       (c) Aleksander Haugas 2024 / MIT Licence ##
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ##
+extends EditorPlugin
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	# Add the new type with a name, a parent type, a script and an icon.
+	add_custom_type("CryptoLib", "Node", preload("res://addons/cryptolib/cyphers/aes256.gd"), preload("res://addons/cryptolib/icon.png"))
+	pass
